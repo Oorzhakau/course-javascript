f00acb6940f17da92e370e8e2611a53ba93a0533
Alexander <oorzhakau@yandex.ru>
2023-11-18 13:51:11 +0300

    week-2: describe methods getRandomElement and getNextPhoto

--- projects/loft-photo/model.js
+++ projects/loft-photo/model.js
@@ -4,6 +4,19 @@ import photosDB from './photos.json';
 import friendsDB from './friends.json';
 
 export default {
-  getRandomElement(array) {},
-  getNextPhoto() {},
+  getRandomElement(array) {
+    if (!array.length) {
+      return null;
+    }
+
+    const randomIndex = Math.round(Math.random() * (array.length - 1));
+    return array[randomIndex];
+  },
+  getNextPhoto() {
+    const friend = this.getRandomElement(friendsDB);
+    const photos = photosDB[friend.id];
+    const photo = this.getRandomElement(photos);
+
+    return { friend: friend, url: photo.url };
+  },
 };
