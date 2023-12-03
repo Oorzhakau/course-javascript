996718685005f5f058ce0bb7830f336f18185163
Alexander <oorzhakau@yandex.ru>
2023-12-03 22:30:44 +0300

    fix structure after review

--- projects/loft-photo/model.js
+++ projects/loft-photo/model.js
@@ -1,5 +1,3 @@
-import subfunc from './subfunc'
-
 const PERM_FRIENDS = 2;
 const PERM_PHOTOS = 4;
 const APP_ID = 51802198;
@@ -17,10 +15,7 @@ export default {
     const friend = this.getRandomElement(this.friends.items);
     const photos = await this.getFriendPhotos(friend.id);
     const photo = this.getRandomElement(photos.items);
-    const size = subfunc.findSize(photo);
-    
-    console.log("this")
-    console.log(this);
+    const size = this.findSize(photo);
     return { friend: friend, id: photo.id, url: size.url };
   },
 
@@ -42,7 +37,7 @@ export default {
 
   async init() {
     this.photoCache = {};
-    this.friends = await subfunc.getFriends();
+    this.friends = await this.getFriends();
   },
 
   photoCache: {},
@@ -52,8 +47,51 @@ export default {
     if (photos) {
       return photos;
     }
-    photos = await subfunc.getPhotos(id);
+    photos = await this.getPhotos(id);
     this.photoCache[id] = photos;
     return photos;
   },
+
+  callAPI(method, params) {
+    params.v = params.v || "5.120";
+
+    return new Promise((resolve, reject) => {
+      VK.api(method, params, (response) => {
+        if (response.error) {
+          reject(new Error(response.error.error_msg));
+        } else {
+          resolve(response.response);
+        }
+      });
+    });
+  },
+
+  getFriends() {
+    const params = {
+      fields: ["photo_50", "photo_100"],
+    }
+    return this.callAPI('friends.get', params);
+  },
+
+  getPhotos(owner) {
+    const params = {
+      owner_id: owner,
+    }
+    return this.callAPI('photos.getAll', params);
+  },
+
+  findSize(photo) {
+    const size = photo.sizes.find((size) => size.width >= 360);
+    
+    if (!size) {
+      return photo.sizes.reduce((biggest, current) => {
+        if (current.width > biggest.width) {
+          return current;
+        }
+        return biggest;
+      }, photo.sizes[0]);
+    }
+
+    return size;
+  }
 };
